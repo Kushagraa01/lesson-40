24f32b952762f55beb980d054fc248a2d9ebb803
Kushagraa01 <swatiabhijain@gmail.com>
2025-03-02 18:07:39 +0530

    lesson 40

---
+++ activity3.py
@@ -0,0 +1,9 @@
+totalsum=sum(range(1,11))
+print("Total sum of 10 natural number is",totalsum)
+ 
+n=int(input("Enter the range "))
+sum1=0
+for i in range(1,n):
+    sum1+=i
+print("Sum of 10 natural numbers is",sum1)
+
